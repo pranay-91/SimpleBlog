7ab19e14a9d737053d81fcdf62f7287ef4cc142a
Pranay Pradhananga <pronoy19114@gmail.com>
2015-07-07 17:49:16 +1000

    Implemented 106 Admin Layout

--- SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog.Infrastructure;
 
 namespace SimpleBlog.Areas.Admin.Controllers
 {
     [Authorize(Roles ="admin")]
+    [SelectedTab("posts")]
     public class PostsController: Controller
     {
-        public ActionResult Index() 
+        public ActionResult Index()
         {
-            return Content("Admin Posts!");
+            return View();
         }
     }
 }
